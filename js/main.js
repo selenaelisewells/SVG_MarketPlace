dc7e83f68513313c3c0d5db5ae34c0c4ab068ba4
Selena Wells <selena.elise.wells@gmail.com>
2020-05-21 19:07:59 -0400

    fix mistake in html

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ let iconSet = document.querySelectorAll('.svgIcon')
 function whenClicked(EventObject) {
     let clickedIcon = EventObject.currentTarget;
 
-    console.log(EventObject.currentTarget)
+    console.log(`the id of the svg clicked is ${clickedIcon.id}`)
 
-    console.log(clickedIcon.id)
+    // console.log(EventObject.currentTarget)
 
-    console.log(EventObject)
+    // console.log(clickedIcon.id)
+
+    // console.log(EventObject)
 
     clickedIcon.classList.toggle('selected')
 }
